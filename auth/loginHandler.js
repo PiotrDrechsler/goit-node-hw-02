c5a5332834319e171a3616b11043df5e8d229c6f
PiotrDrechsler <pdrech@gmail.com>
2023-03-30 19:18:33 +0200

    LOGIN Routes + LoginHandler
    jsonwebtoken:
    - installed
    LOGIN Routes:
    - added post
    LoginHandler:
    - added loginHandler.js + all logic
    IssueToken:
    -added issueToken.js + all logic

---
+++ auth/loginHandler.js
@@ -0,0 +1,17 @@
+const bcrypt = require("bcrypt");
+
+const { getUserByEmail } = require("../controllers/users");
+const issueToken = require("./issueToken");
+
+const loginHandler = async (email, incomingPassword) => {
+  const user = await getUserByEmail(email);
+  const userPassword = user.password;
+  const result = bcrypt.compareSync(incomingPassword, userPassword);
+  if (result) {
+    return issueToken(user);
+  } else {
+    throw "Invalid credentials";
+  }
+};
+
+module.exports = loginHandler;
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const contactRouter = require("./contacts");
 const signupRouter = require("./signup");
+const loginRouter = require("./login");
 
 const router = express.Router();
 
 router.use("/api/contacts", contactRouter);
 router.use("/users/signup", signupRouter);
+router.use("/users/login", loginRouter);
 
 module.exports = router;
